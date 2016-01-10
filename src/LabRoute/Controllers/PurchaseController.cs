9c5a7b2bbb958d36b931519ad51fa49209a5f74f
MrXtry <larsson__96@hotmail.com>
2016-01-10 14:49:08 +0100

    LabRoute
    All files for the lab

---
+++ src/LabRoute/Controllers/PurchaseController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace LabRoute.Controllers
+{   
+    [Route("orders")]
+    public class PurchaseController : Controller
+    {
+        [Route("index")]
+        public IActionResult ShowAll()
+        {
+            return View();
+        }
+        [Route("details/{id:int}")]
+        public IActionResult Show(int id)
+        {
+            return View();
+        }
+    }
+}
